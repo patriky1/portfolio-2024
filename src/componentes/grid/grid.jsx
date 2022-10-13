6e261b58fc6181475b77de2de497a8ed2cbc6604
patriky brito <patrikybrito@gmail.com>
2022-10-12 23:29:14 -0300

    init

---
+++ src/componentes/grid/grid.jsx
@@ -0,0 +1,21 @@
+
+import './grid.css';
+import React from 'react'
+
+function grid() {
+  return (
+    <>
+    <div className="quadrado"></div>
+    <div className="elipse"></div>
+    <div className="quadrado2"></div>
+    <div className="elipse2"></div>
+    <div className="quadrado4"></div>
+    <div className="quadrado3"></div>
+    <div className='text1'><h1>Lorem Ipisun</h1></div>
+    <div className="line1"></div>
+    <div className="line2"></div>
+    </>
+  )
+}
+
+export default grid
