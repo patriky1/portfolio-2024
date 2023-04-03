378e9ce42d16f1999fe3efa6feff6268a24bd8a8
patriky1 <patrikybrito@gmail.com>
2023-04-03 00:59:06 -0300

    feat: add new grids

--- src/componentes/grid/grid.jsx
+++ src/componentes/grid/grid.jsx
@@ -14,161 +14,139 @@ import bar3 from "../imagens/bar2.png";
 import projects from "../imagens/projects.png";
 // import RightContact from "./contact";
 
+function Name() {
+  return (
+    <div className="name">
+      <h4 className="title">Patriky</h4>
+      <h4 className="title1"> Brito</h4>
+    </div>
+  );
+}
 export default function App() {
   return (
     <div>
-
+      <Name></Name>
       <Carousel fade controls={false} interval={null}>
-        <Carousel.Item  >
-          <div className="bg1">
-            <h4 className="title">Patriky</h4>
-            <h4 className="title1"> Brito</h4>
-          </div>
+        <Carousel.Item>
+          <div className="bg1"></div>
           <div className="retangle1"></div>
           <h1 className="titleprincipal">
             Ajudando<br></br> Negócios e <br></br> Organizações a Crescer
           </h1>
-          <p className="info">
-            E patrikybrito@gmail.com <br></br> T +55 83 9697-9777 <br></br>{" "}
-            ©2022{" "}
-          </p>
-          <Carousel.Caption >
+
+          <Carousel.Caption>
             <p className="title2">Desenvolvedor </p>
             <p className="titlegreen">Front-end</p>
           </Carousel.Caption>
         </Carousel.Item>
 
         <Carousel.Item interval={null}>
-          <div className="bg1">
-            <h4 className="title">Patriky</h4>
-            <h4 className="title1"> Brito</h4>
-          </div>
+          <div className="bg1"></div>
 
-          <p className="info">
-            E patrikybrito@gmail.com <br></br> T +55 83 9697-9777 <br></br>{" "}
-            ©2022{" "}
-          </p>
           <div className="sobre">
-          <h3>Sobre mim</h3>
+            <h3>Sobre mim</h3>
           </div>
           <div className="sobremim">
-          <h5 className="sobremim">
-            Sou o Patriky Brito, tenho 24 anos e sou Desenvolvedor Front-end.
-            Meu objetivo é entregar experiências memoráveis para novas marcas e
-            para aqueles que desejam digitalizar-se. Trabalho como programador
-            desde 2019, e me considero um profissional autodidata com visão
-            estratégica, grande adaptabilidade e bom fortalecimento de equipes.
-            Me graduei na Área de Redes de Computadores (UNIP). Atualmente
-            trabalhando como desenvolvedor front-end na Lane art design e
-            Formador de profissionais ágeis que buscam adentar no mercado da
-            programação. <br></br>No meu tempo livre, gosto de experimentar
-            cachaças artesanais, cozinhar novos pratos, viajar e fazer
-            caminhadas ao ar livre.
-          </h5>
+            <h5 className="sobremim">
+              Sou o Patriky Brito, tenho 24 anos e sou Desenvolvedor Front-end.
+              Meu objetivo é entregar experiências memoráveis para novas marcas
+              e para aqueles que desejam digitalizar-se. Trabalho como
+              programador desde 2019, e me considero um profissional autodidata
+              com visão estratégica, grande adaptabilidade e bom fortalecimento
+              de equipes. Me graduei na Área de Redes de Computadores (UNIP).
+              Atualmente trabalhando como desenvolvedor front-end na Lane art
+              design e Formador de profissionais ágeis que buscam adentar no
+              mercado da programação. <br></br>No meu tempo livre, gosto de
+              experimentar cachaças artesanais, cozinhar novos pratos, viajar e
+              fazer caminhadas ao ar livre.
+            </h5>
           </div>
           <div className="qrcode">
             <img className="qrcode" src={qrcode} alt="qrcode" />
           </div>
-          
         </Carousel.Item>
 
-
-
         <Carousel.Item interval={null}>
-          <div className="bg1">
-            <h4 className="title">Patriky</h4>
-            <h4 className="title1"> Brito</h4>
-          </div>
+          <div className="bg1"></div>
           <h1 className="informacoes">Habilidades Profissionais</h1>
-          <div className="imgbar1" >
-            <img className="imgbar1" src={imgbar} alt="imgbar"  />
+          <div className="imgbar1">
+            <img className="imgbar1" src={imgbar} alt="imgbar" />
           </div>
-          <p className="info">
-            E patrikybrito@gmail.com <br></br> T +55 83 9697-9777 <br></br>{" "}
-            ©2022{" "}
-          </p>
         </Carousel.Item>
 
-
-
         <Carousel.Item interval={null}>
-          <div className="bg1">
-            <h4 className="title">Patriky</h4>
-            <h4 className="title1"> Brito</h4>
-          </div>
+          <div className="bg1"></div>
           <h1 className="informacoes">Idiomas</h1>
-       
-              <div className="portugues">
-                <h5 > Português</h5>
-              </div>
-            <div className="bar1" >
-            <img  src={bar1} className="bar1" alt="bar1" />
-            </div>
-            <div className="espanhol">
-             <h5> Espanhol </h5>
-            </div>
-            <div className="bar2">
-            <img  className="bar2" src={bar1} alt="bar1" />
-            </div>
-            <div className="ingles">
-            <h5 > Inglês</h5>
-            </div>
-            <div className="bar3">
+
+          <div className="portugues">
+            <h5> Português</h5>
+          </div>
+          <div className="bar1">
+            <img src={bar1} className="bar1" alt="bar1" />
+          </div>
+          <div className="espanhol">
+            <h5> Espanhol </h5>
+          </div>
+          <div className="bar2">
+            <img className="bar2" src={bar1} alt="bar1" />
+          </div>
+          <div className="ingles">
+            <h5> Inglês</h5>
+          </div>
+          <div className="bar3">
             <img className="bar3" src={bar3} alt="bar1" />
-            </div>
-          
-          <p className="info">
-            E patrikybrito@gmail.com <br></br> T +55 83 9697-9777 <br></br>{" "}
-            ©2022{" "}
-          </p>
+          </div>
           <div className="formacao">
-           <h3> Formação Acadêmica</h3>
+            <h3> Formação Acadêmica</h3>
           </div>
           <div className="exp">
             <h5 className="exp">
-            Graduado em Redes de computadores pela Universidade Paulista UNIP,
-            Sousa-PB Técnico em Informática, Instituto Federal de Ciências e
-            Técnologia da Paraíba - IFPB. Ensino médio Integrado ao Técnico em
-            Informática -IFPB.
+              Graduado em Redes de computadores pela Universidade Paulista UNIP,
+              Sousa-PB Técnico em Informática, Instituto Federal de Ciências e
+              Técnologia da Paraíba - IFPB. Ensino médio Integrado ao Técnico em
+              Informática -IFPB.
             </h5>
-            </div>
-            <div className="formacao1">
-            <h3 >Experiência profissional</h3>
-            </div>
+          </div>
+          <div className="formacao1">
+            <h3>Experiência profissional</h3>
+          </div>
           <div className="exp1">
             <h5 className="exp1">
-            Especialista em análise de dados no Instituto Brasileiro de
-            Geografia e Estatistica - IBGE Desenvolvedor Front-end, LANE ART
-            DESIGN Docente em Programação de sistemas, E.N.E. Jose de Paiva
-            Gadelha
+              Especialista em análise de dados no Instituto Brasileiro de
+              Geografia e Estatistica - IBGE Desenvolvedor Front-end, LANE ART
+              DESIGN Docente em Programação de sistemas, E.N.E. Jose de Paiva
+              Gadelha
             </h5>
           </div>
-          
         </Carousel.Item>
-
-
 
         <Carousel.Item interval={null}>
-          <div className="bg1">
-            <h4 className="title">Patriky</h4>
-            <h4 className="title1"> Brito</h4>
-          </div>
+          <div className="bg1"></div>
           <div className="informacoes">
-          <h3>Projetos</h3>
+            <h3>Projetos</h3>
           </div>
-          <div className="projects" >
-          <img className="projects" src={projects} alt="projects" />
+          <div className="projects">
+            <IndividualIntervalsExample></IndividualIntervalsExample>
           </div>
-          <p className="info">
-            E patrikybrito@gmail.com <br></br> T +55 83 9697-9777 <br></br>{" "}
-            ©2022{" "}
-          </p>
-
         </Carousel.Item>
-
-
-
       </Carousel>
+      <p className="info">
+        E patrikybrito@gmail.com <br></br> T +55 83 9697-9777 <br></br> ©{year}{" "}
+      </p>
     </div>
   );
 }
+function IndividualIntervalsExample() {
+  return (
+    <Carousel>
+      <Carousel.Item interval={500}>
+        <img className="d-block w-100" src={projects} alt="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+const year = new Date().getFullYear();
