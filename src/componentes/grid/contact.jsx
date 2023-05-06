c4f15d2301445098c5c901f400dca4460925dcbe
patriky1 <106018532+patriky1@users.noreply.github.com>
2023-05-05 22:13:31 -0300

    Update contact.jsx

--- src/componentes/grid/contact.jsx
+++ src/componentes/grid/contact.jsx
@@ -42,7 +42,35 @@ position: absolute;
     height: 0px;
     border: 1px solid #ffffff;
     transform: rotate(90deg);
-  }`
+  }
+  @media screen and (max-width: 780px) {
+    position: absolute;
+    display: flex;
+    flex-direction:row;
+    justify-content: center;
+    align-items: center;
+    bottom: 0vh;
+    gap: 1.25rem;
+    .p-right, .line {
+    transform: rotate(0deg);
+    margin: 0;
+    }
+    .icons-content {
+      flex-direction: row;
+      padding: 0px;
+    margin: 0px;
+    gap: 35px;
+    right: 0;
+    max-height:20px;
+    * {
+      aspect-ratio:1;
+      width: 50px;
+      heigth: 50px;
+    }
+    }
+  }
+}
+  `
 ;
 
 export default function RightContact() {
@@ -52,15 +80,15 @@ export default function RightContact() {
         <span className="p-right">Contato</span>
         <p className="line"></p>
         <div className="icons-content">
-          <img
-            className="email"
+        <a href="#" className="email"><img
             src={img1}
             href="mailto:patrikybrito@gmail.com"
             alt="email"
-          />
-          <img className="linkedin" src={img2} alt="linkedin" />
-          <img className="insta" src={img3} alt="instagram" />
-          <img className="git" src={img4} alt="github" />
+          /></a>
+          
+          <a href="#" className="linkedin"><img src={img2} alt="linkedin" /></a>
+          <a href="#" className="insta"><img src={img3} alt="instagram" /></a>
+          <a href="#" className="git"><img src={img4} alt="github" /></a>
         </div>
       </Contact>
     </>
